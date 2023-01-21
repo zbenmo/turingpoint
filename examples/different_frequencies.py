f0208deea13a77696c58fd461adb9a8e44a5c946
Oren <zbenmo@gmail.com>
2023-01-21 16:36:53 +0100

    added different_frequencies example

---
+++ examples/different_frequencies.py
@@ -0,0 +1,39 @@
+from turingpoint.assembly import Assembly
+from turingpoint.definitions import Participant
+from turingpoint.utils import print_parcel
+from typing import List, Generator
+
+
+def diffrent_frequencies():
+  
+  def participant1(parcel: dict) -> None:
+    parcel['participant1'] = parcel.get('participant1', 0) + 1
+
+  def participant2(parcel: dict) -> None:
+    parcel['participant2'] = parcel.get('participant2', 0) + 1
+
+  def participant3(parcel: dict) -> None:
+    parcel['participant3'] = parcel.get('participant3', 0) + 1
+
+  class MyAssembly(Assembly):
+    def create_initial_parcel(self) -> dict:
+      return {}
+
+    def participants(self) -> Generator[Participant, None, None]:
+      for i in range(20):
+        if i % 1 == 0:
+          yield participant1
+        if i % 2 == 0:
+          yield participant2
+        if i % 3 == 0:
+          yield participant3
+        yield print_parcel
+
+  assembly = MyAssembly()
+  parcel = assembly.launch()
+
+  print(f'{parcel=}')
+
+
+if __name__ == "__main__":
+  diffrent_frequencies()
--- turingpoint/__init__.py
+++ turingpoint/__init__.py
@@ -1,7 +1,7 @@
 from .assembly import Assembly
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = [
   "Assembly"
