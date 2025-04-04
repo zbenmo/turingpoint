3f3463a9171cbff27afda202ad14588283f3cfd2
Oren <zbenmo@gmail.com>
2025-04-04 17:09:29 +0200

    added Optuna

--- examples/humanoid-gym-torch-ddpg.py
+++ examples/humanoid-gym-torch-ddpg.py
@@ -1,8 +1,10 @@
+import ast
+import copy
 import datetime
 import functools
 import itertools
 import random
-from typing import Dict
+from typing import Dict, Tuple
 import numpy as np
 import gymnasium as gym
 from gymnasium.utils.save_video import save_video
@@ -12,6 +14,7 @@ from torch.nn import functional as F
 from torch.optim.lr_scheduler import ExponentialLR
 from tqdm import trange
 from contextlib import contextmanager
+import optuna
 
 import turingpoint.gymnasium_utils as tp_gym_utils
 import turingpoint.utils as tp_utils
@@ -21,11 +24,11 @@ import turingpoint as tp
 
 
 gym_environment = 'Humanoid-v5'
-use_batch_normilization = False
 
 
 class StateToAction(nn.Module):
     def __init__(self, in_features, out_actions, env, *args, **kwargs):
+        use_batch_normilization = kwargs.pop('use_batch_normilization', None)
         super().__init__(*args, **kwargs)
         layers = []
         in_f = in_features
@@ -50,18 +53,19 @@ class StateToAction(nn.Module):
     def forward(self, obs: torch.Tensor) -> torch.Tensor:
         """obs -> action (regression)"""
 
-        # return F.tanh(self.net(obs)) * 4.0
-        x = self.net(obs)
-        return x
+        return F.tanh(self.net(obs)) * 4.0
+        # x = self.net(obs)
         # return x * self.action_scale + self.action_bias # (copied from https://docs.cleanrl.dev/rl-algorithms/ddpg/#implementation-details)
 
 
 class StateActionToQValue(nn.Module):
     def __init__(self, in_features, in_actions, *args, **kwargs):
+        use_batch_normilization = kwargs.pop('use_batch_normilization', None)
+        layers_critic = kwargs.pop('layers_critic', None)
         super().__init__(*args, **kwargs)
         layers = []
         in_f = in_features + in_actions
-        for out_f in [400, 300]:
+        for out_f in layers_critic:
             if use_batch_normilization:
                 layers.append(nn.BatchNorm1d(in_f))
             layers.append(nn.Linear(in_features=in_f, out_features=out_f))
@@ -113,6 +117,17 @@ def evaluate(env, agent, num_episodes: int) -> float:
         # Note that we don't clear the rewards in 'rewards_collector', and so we continue to collect.
 
     total_reward = sum(x['reward'] for x in rewards_collector.get_entries())
+    count = sum(1 for _ in rewards_collector.get_entries())
+
+    rewards = [x['reward'] for x in rewards_collector.get_entries()]
+
+    print(f'{np.mean(rewards)=}')
+    print(f'{np.max(rewards)=}')
+    print(f'{np.min(rewards)=}')
+
+    print(f'{count=}')
+    print(f'{total_reward=}')
+    print(f'{num_episodes=}')
 
     return total_reward / num_episodes
 
@@ -127,23 +142,21 @@ def start_train(model: nn.Module):
         model.eval()
 
 
-def train(env, actor: StateToAction, critic: StateActionToQValue, total_timesteps):
+def train(optuna_trial, env, actor: StateToAction, critic: StateActionToQValue, total_timesteps):
     """Given a model (agent) and a critic. Train the model (and the critic) for 'total_timesteps' steps."""
 
     rendering_env = gym.make(gym_environment, render_mode="rgb_array_list")
 
-    state_space = env.observation_space.shape[0] # alternatively take it from agent/target...
-    action_space = env.action_space.shape[-1]
-    target_actor = StateToAction(state_space, out_actions=action_space, env=env)
-    target_critic = StateActionToQValue(state_space, action_space)
-    target_actor.load_state_dict(actor.state_dict()) # initialize the policy and the target with the same (random) values
-    target_critic.load_state_dict(critic.state_dict()) # same here
-
     actor.eval() # when we'll actually train, we'll say it explicitly below (in learn)
     critic.eval() # same here
 
+    target_actor = copy.deepcopy(actor)
+    target_critic = copy.deepcopy(critic)
+
     target_actor.eval()
     target_critic.eval()
+
+    use_batch_normilization = optuna_trial.params['use_batch_normilization']
 
     if use_batch_normilization:
         actor_batch_norm_stats = tp_torch_utils.get_parameters_by_name(actor, ["running_"])
@@ -151,20 +164,23 @@ def train(env, actor: StateToAction, critic: StateActionToQValue, total_timestep
         actor_batch_norm_stats_target = tp_torch_utils.get_parameters_by_name(target_actor, ["running_"])
         critic_batch_norm_stats_target = tp_torch_utils.get_parameters_by_name(target_critic, ["running_"])
 
-    discount = 0.995 # AKA: gamma
-    gradient_steps = 2
-    batch_size = 256
-    learning_starts = 25_000
-    replay_buffer_size = 1_000_000
-    policy_delay = 2
+    discount = optuna_trial.suggest_categorical("discount", [0.995]) # AKA: gamma
+    gradient_steps = optuna_trial.suggest_categorical("gradient_steps", [2])
+    batch_size = optuna_trial.suggest_categorical("batch_size", [256])
+    learning_starts = optuna_trial.suggest_categorical("learning_starts", [25_000])
+    replay_buffer_size = optuna_trial.suggest_categorical("replay_buffer_size", [1_000_000])
+    policy_delay = optuna_trial.suggest_categorical("policy_delay", [2])
 
     replay_buffer_collector = tp_utils.ReplayBufferCollector(
         collect=['obs', 'action', 'reward', 'terminated', 'truncated', 'next_obs'], max_entries=replay_buffer_size)
 
     per_episode_rewards_collector = tp_utils.Collector(['reward'])
 
-    optimizer_agent = torch.optim.Adam(actor.parameters(), lr=1e-4)
-    optimizer_critic = torch.optim.Adam(critic.parameters(), lr=1e-3)
+    lr_agent = optuna_trial.suggest_categorical("lr_agent", [1e-4])
+    lr_critic = optuna_trial.suggest_categorical("lr_critic", [1e-3])
+
+    optimizer_agent = torch.optim.Adam(actor.parameters(), lr=lr_agent)
+    optimizer_critic = torch.optim.Adam(critic.parameters(), lr=lr_critic)
     # scheduler_agent = ExponentialLR(optimizer_agent, gamma=0.99)
     # scheduler_critic = ExponentialLR(optimizer_critic, gamma=0.99)
 
@@ -172,7 +188,7 @@ def train(env, actor: StateToAction, critic: StateActionToQValue, total_timestep
     # min_epsilon = 0.05
 
     def update_target(target_agent, target_critic, agent, critic):
-        tau = 0.001 # if needed, can use two different values
+        tau = optuna_trial.suggest_categorical("tau", [0.001]) # if needed, can use two different values
         tp_torch_utils.polyak_update(agent.parameters(), target_agent.parameters(), tau) # agent -> target_agent
         tp_torch_utils.polyak_update(critic.parameters(), target_critic.parameters(), tau) # critic -> target_critic
         if use_batch_normilization:
@@ -287,8 +303,8 @@ def train(env, actor: StateToAction, critic: StateActionToQValue, total_timestep
             # here is what we've actually came to do
             tp_gym_utils.call_reset(parcel, env=env)
 
-    # def take_interesting_info(parcel: dict):
-    #     parcel.update(parcel.pop('info'))
+    def take_interesting_info(parcel: dict):
+        parcel.update(parcel.pop('info'))
 
     def record_video(parcel: dict):
         """When called, either returns immediatly, or renders a one episode video."""
@@ -331,8 +347,9 @@ def train(env, actor: StateToAction, critic: StateActionToQValue, total_timestep
                 # 'action',
                 # 'noise',
 
-                # 'reward_survive',
-                # 'reward_forward',
+                'reward_survive',
+                'reward_forward',
+                'reward_ctrl',
 
                 # 'Loss(agent)+std/train',
                 # 'Loss(agent)-std/train',
@@ -350,7 +367,7 @@ def train(env, actor: StateToAction, critic: StateActionToQValue, total_timestep
                 replay_buffer_collector,
                 learn,
                 per_episode_rewards_collector,
-                # take_interesting_info, # those will be also logged.
+                take_interesting_info, # those will be also logged.
                 logging,
                 record_video,
                 steps_tracker, # can raise Done
@@ -363,14 +380,31 @@ def train(env, actor: StateToAction, critic: StateActionToQValue, total_timestep
     train_assembly.launch()
 
 
-def main():
+def create_networks(state_space, action_space, env, use_batch_normilization, layers_critic) -> Tuple[StateToAction, StateActionToQValue]:
+    act = StateToAction(
+        state_space,
+        out_actions=action_space,
+        env=env,
+        use_batch_normilization=use_batch_normilization
+    ) # This is the agent
+    critic = StateActionToQValue(
+        state_space,
+        action_space,
+        use_batch_normilization=use_batch_normilization,
+        layers_critic=layers_critic
+    ) # This is the Q value
+    return act, critic
 
-    random.seed(1)
-    np.random.seed(1)
-    torch.manual_seed(1)
 
-    env = gym.make(gym_environment)
-    env.reset(seed=1)
+def create_networks_with_optuna_trial(optuna_trial, state_space, action_space, env):
+    use_batch_normilization = optuna_trial.suggest_categorical("use_batch_normilization", [False])
+    layers_critic = optuna_trial.suggest_categorical("layers_critic", ["[300, 400, 300, 400]"])
+    layers_critic = ast.literal_eval(layers_critic)
+
+    return create_networks(state_space, action_space, env, use_batch_normilization, layers_critic)
+
+
+def optuna_objective(optuna_trial, env):
 
     # state and obs/observations are used in this example interchangably.
 
@@ -380,8 +414,12 @@ def main():
     # print(f'{state_space=}')
     # print(f'{action_space=}')
 
-    act = StateToAction(state_space, out_actions=action_space, env=env) # This is the agent
-    critic = StateActionToQValue(state_space, action_space)
+    act, critic = create_networks_with_optuna_trial(
+        optuna_trial=optuna_trial,
+        state_space=state_space,
+        action_space=action_space,
+        env=env
+    )
 
     name_of_model_file_act = 'act_state.pth'
     name_of_model_file_critic = 'critic_state.pth'
@@ -394,7 +432,8 @@ def main():
     print("before training")
     print(f'{mean_reward_before_train=}')
 
-    train(env, act, critic, total_timesteps=1_000_000)
+    total_timesteps = optuna_trial.suggest_categorical("total_timesteps", [40_000])
+    train(optuna_trial, env, act, critic, total_timesteps=total_timesteps)
 
     mean_reward_after_train = evaluate(env, act, 100)
     print("after training")
@@ -402,6 +441,29 @@ def main():
 
     torch.save(act.state_dict(), name_of_model_file_act)
     torch.save(critic.state_dict(), name_of_model_file_critic)
+
+    return mean_reward_after_train
+
+
+def main():
+
+    random.seed(1)
+    np.random.seed(1)
+    torch.manual_seed(1)
+
+    sqlite_file = 'optuna_trials.db'
+    storage = f'sqlite:///{sqlite_file}'
+    optuna_study = optuna.create_study(
+        storage=storage,
+        study_name=f'{gym_environment} DDPG',
+        direction="maximize",
+        load_if_exists=True,
+    )
+
+    env = gym.make(gym_environment)
+    env.reset(seed=1)
+
+    optuna_study.optimize(functools.partial(optuna_objective, env=env), n_trials=1)
 
 
 if __name__ == "__main__":
